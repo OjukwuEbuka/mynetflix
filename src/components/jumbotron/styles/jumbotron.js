0cdb2b2de5de0a0fc9e170150d71afc7758ad604
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-11-29 21:20:00 +0100

    Completed Jumbotron Compound Component and added globalstyle

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -1,12 +1,20 @@
 import styled from 'styled-components/macro';
 
+export const Item = styled.div`
+    display: flex;
+    border-bottom: 8px solid #222;
+    padding: 50px 5%;
+    color: white;
+    overflow: hidden;
+`;
+
 export const Inner = styled.div`
     display: flex;
     align-items: center;
-    flex-direction: ${({ direction }) => direction}
+    flex-direction: ${({ direction }) => direction};
     justify-content: space-between;
     max-width: 1100px;
-    margin: auto:
+    margin: auto;
     width: 100%;
 
     @media (max-width: 1000px) {
@@ -16,12 +24,43 @@ export const Inner = styled.div`
 
 export const Pane = styled.div`
     width: 50%;
+
+    @media(max-width: 1000px){
+        width: 100%;
+        padding: 0 45px;
+        text-align: center;
+    }
 `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+    font-size: 50px;
+    line-height: 1.1;
+    margin-bottom: 8px;
 
-export const SubTitle = styled.h2``;
+    @media (max-width: 600px){
+        font-size: 35px;
+    }
+`;
 
-export const Image = styled.img``;
+export const SubTitle = styled.h2`
+    fonst-size: 26px;
+    font-weight: normal;
+    line-height: normal;
 
-export const Container = styled.div``;
+    @media(max-width: 600px){
+        font-size: 18px;
+    }
+`;
+
+export const Image = styled.img`
+    max-width: 100%;
+    height: auto;
+`;
+
+export const Container = styled.div`
+    @media (max-width: 1000px){
+        ${Item}:last-of-type h2 {
+            margin-bottom: 50px;
+        }
+    }
+`;
