6a0f2945b295caf818b0b98052fc69a317f3d648
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-12-06 20:36:32 +0100

    Created the Header component with reuse of OptForm

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { OptForm, Feature } from '../components';
+import { HeaderContainer } from '../containers/header';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
 import {JumbotronContainer} from '../containers/jumbotron';
 
 export default function Home() {
-    return <>
+    return (<>
+        <HeaderContainer>
+            <Feature>
+                <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                <Feature.SubTitle>Watch anywhere. Cancel att any time.</Feature.SubTitle>
+                <OptForm>
+                    <OptForm.Input placeholder="Email address" />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>
+                        Ready to watch? Enter your email to create or restart your membership
+                    </OptForm.Text>
+                </OptForm>
+            </Feature>
+        </HeaderContainer>
         <JumbotronContainer />
         <FaqsContainer />
         <FooterContainer />
-    </>
+    </>)
 }
