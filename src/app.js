ffde669d9cf7d8a11c440dab10691acf1f96a786
OjukwuEbuka <ebukaojukwu23@gmail.com>
2020-12-06 15:04:08 +0100

    Created FAQs-Accordion compound component

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Faqscontainer } from './containers/faqs';
 import { FooterContainer } from './containers/footer';
 import {JumbotronContainer} from './containers/jumbotron';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <JumbotronContainer />
+      <Faqscontainer />
       <FooterContainer />
     </>
   );
